c7a41317684126a7fcc22dfadda94028bf91d783
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-11 00:55:47 +0100

    added more to navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import Logo from "../assets/logo.png"
 
 export default function NavBar() {
     return (
-        <div className="">
-            Hello Navbar
+        <div className="flex h-16 m-2 justify-around items-center">
+            <img src={Logo} alt="PowerQuiz logo" className="h-16" />
+            <svg className="w-6 h-6 sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            <div className="hidden sm:flex justify-evenly gap-5 font-semibold max-h-8 items-center">
+              
+                <button className="text-slate-500 hover:text-black transition-colors">Login</button>
+                <button className="bg-main rounded-full p-2 px-4 text-white">Get Started</button>
+            </div>
         </div>
     )
 }

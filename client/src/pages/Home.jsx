62661b906805e81c2c6253efa2786120fefd970b
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-11 00:56:17 +0100

    added features and new components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,28 @@
 import NavBar from "../components/Navbar"
+import HeroSection from "../components/Hero"
+import StackFeatureDisplay from "../components/StackFeatureDisplay"
+
+const featureDisplay = [
+    {
+        headline: "Create beautiful quizzes quickly.",
+        info: "Use our simple quiz creator to whip up quizzes on the fly within minutes!"
+    },
+    {
+        headline: "Create beautiful quizzes quickly.",
+        info: "Use our simple quiz creator to whip up quizzes on the fly within minutes!"
+    },
+    {
+        headline: "Create beautiful quizzes quickly.",
+        info: "Use our simple quiz creator to whip up quizzes on the fly within minutes!"
+    },
+]
 
 export default function HomePage() {
    return(
     <>
         <NavBar />
+        <HeroSection />
+        <StackFeatureDisplay features={featureDisplay}/>
     </>
     )
 }

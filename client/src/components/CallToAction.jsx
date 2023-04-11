11d1f7306bafb162684b9e26d49eda7d29905fb4
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-11 04:22:28 +0100

    created call to action component

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,11 @@
+export default function CallToAction( { action, onButtonClick } ) {
+    return(
+        <div className="flex flex-wrap justify-center gap-20 items-start mb-10">
+            <h1 className="text-6xl font-bold">{action.heading[0]}<br /> {action.heading[1]}</h1>
+            <div className="flex flex-col max-w-lg gap-8">
+                <p className="text-xl font-medium">{action.body}</p>
+                <button className="bg-main text-white font-semibold h-16 py-2 px-6 rounded-full w-1/2">Sign Up for free</button> 
+            </div>
+        </div>
+    )
+}

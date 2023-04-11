1b62f059ea9b49b14133dd87b834009b87704289
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-11 03:57:58 +0100

    added new components and props

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,57 @@
+// components
 import NavBar from "../components/Navbar"
 import HeroSection from "../components/Hero"
-import StackFeatureDisplay from "../components/StackFeatureDisplay"
+import FeatureDisplay from "../components/FeatureDisplay"
+import QuoteCard from "../components/QuoteCard"
 
+// images for features
+import imgGroup from "../images/group.png"
+import imgPencil from "../images/pencil.png"
+import imgStickyNote from "../images/stickynote.png"
+
+// images for quotes
+import imgMale1 from "../images/teacher.png"
+import imgMale2 from "../images/teacher1.png"
+import imgFemale from "../images/teacher2.png"
+
+// features for component
 const featureDisplay = [
     {
-        headline: "Create beautiful quizzes quickly.",
-        info: "Use our simple quiz creator to whip up quizzes on the fly within minutes!"
+        headline: "Save time with automated grading!",
+        info: "Simple pop quizzes can be automatically graded so students get instant feedback, and you save time marking.",
+        img: imgPencil
     },
     {
-        headline: "Create beautiful quizzes quickly.",
-        info: "Use our simple quiz creator to whip up quizzes on the fly within minutes!"
+        headline: "Create beautiful quizzes quickly!",
+        info: "Use our simple quiz creator to whip up quizzes on the fly within minutes and send them out to your students!",
+        img: imgStickyNote
     },
     {
-        headline: "Create beautiful quizzes quickly.",
-        info: "Use our simple quiz creator to whip up quizzes on the fly within minutes!"
+        headline: "Get feedback from students.",
+        info: "Students can give feedback right after completing their quiz, you can view that alongside analytical data at your dashboard.",
+        img: imgGroup
+    },
+]
+
+// quotes for component
+const quotes = [
+    {
+        heading: "Richard",
+        subheading: "Year 9 Teacher",
+        body: "The automated grading feature has been a lifesaver. It saves me so much time and ensures that grading is fair and accurate. Plus it gives my students instant feedback.",
+        img: imgMale1
+    },
+    {
+        heading: "Laura",
+        subheading: "Year 11 Teacher",
+        body: "I love the flexibility of this quiz creator! It allows me to create quizzes with different question types like multiple choice questions and questions where text is required.",
+        img: imgFemale
+    },
+    {
+        heading: "Richard",
+        subheading: "Grade 12 Teacher",
+        body: "The analytics dashboard is fantastic. It gives me valuable insights into how my students are doing and helps me tailor my lessons to their needs.",
+        img: imgMale2
     },
 ]
 
@@ -22,7 +60,15 @@ export default function HomePage() {
     <>
         <NavBar />
         <HeroSection />
-        <StackFeatureDisplay features={featureDisplay}/>
+        <FeatureDisplay features={featureDisplay}/>
+        <h1 className="text-center text-6xl font-bold mt-36">
+            See what others had to say about us.
+        </h1>
+        <div className="flex justify-center gap-12 mt-10 mb-24 flex-wrap p-4">
+            <QuoteCard quote={quotes[0]}/>
+            <QuoteCard quote={quotes[1]}/>
+            <QuoteCard quote={quotes[2]}/>
+        </div>
     </>
     )
 }

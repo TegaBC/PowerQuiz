f5207aed37b79f3b55a220a2fc6583e5a2c20fef
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-12 22:32:51 +0100

    setting up mongodb

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,33 @@
+// Dependencies
 import express from "express"
+import mongoose from "mongoose"
 import dotenv from "dotenv"
 import cors from "cors"
+
+// Routers
+import registerRouter from "./routes/register.js"
+
+// Setup env, and express
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5050
 
+// middleware
 app.use(cors)
 
+// routes
+app.use("/register", registerRouter)
+
+// Connect to mongodb
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connected to mongo DB successfully"))
+    .catch(err => console.log(err));
+
+// start
 app.listen(port, () => {
     console.log("Server is listening on port " + port)
 })

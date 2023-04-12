5b27d5c16fca008ef544d81388d9fdc5b4ec4696
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-12 18:19:56 +0100

    added quotations to quotes

--- client/src/components/QuoteCard.jsx
+++ client/src/components/QuoteCard.jsx
@@ -7,7 +7,7 @@ export default function QuoteCard( { quote } ) {
             <img className=" max-w-[120px] object-cover aspect-square rounded-full" src={quote.img} alt="Image of person for testimonial." />
             <h1 className="font-bold text-2xl mt-2">{quote.heading}</h1>
             <h2>{quote.subheading}</h2>
-            <p className="mt-4 max-w-lg text-lg font-medium max-w-[20rem] text-center">{quote.body}</p>
+            <p className="mt-4 max-w-lg text-lg font-medium max-w-[20rem] text-center">"{quote.body}"</p>
         </div>
     )
 }

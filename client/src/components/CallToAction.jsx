2bcc5375fc122ab9a4ed40424b9e947057adb1a8
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-12 19:08:12 +0100

    render button or link element

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,12 +1,29 @@
+import { Link } from "react-router-dom";
 
+// Renders either a button with a function or a link tag to an internal domain
 
-export default function CallToAction( { action, onButtonClick } ) {
+function renderAction(actionInfo, action) {
+    if (typeof action === "function") {
+        return (<button onClick={action} className="bg-main rounded-full py-5 px-16 text-white font-semibold transition-colors hover:bg-main2">
+            {actionInfo.buttonText}
+            </button>
+        )
+    } else {
+        return (
+            <Link to={action} className="bg-main rounded-full py-5 px-16 text-white font-semibold transition-colors hover:bg-main2">
+                {actionInfo.buttonText}
+            </Link>
+        )
+    }
+}
+
+export default function CallToAction( { actionInfo, action} ) {
     return(
         <div className="flex flex-wrap justify-center gap-20 items-start mb-10">
-            <h1 className="text-6xl font-bold text-center">{action.heading[0]}<br /> {action.heading[1]}</h1>
+            <h1 className="text-6xl font-bold text-center">{actionInfo.heading[0]}<br /> {actionInfo.heading[1]}</h1>
             <div className="flex flex-col items-center max-w-lg gap-8">
-                <p className="text-xl font-medium text-center">{action.body}</p>
-                <button className="bg-main text-white font-semibold h-16 py-2 px-6 rounded-full w-1/2 transition-colors hover:bg-main2">{action.buttonText}</button> 
+                <p className="text-xl font-medium text-center">{actionInfo.body}</p>
+                { renderAction(actionInfo, action) } 
             </div>
         </div>
     )

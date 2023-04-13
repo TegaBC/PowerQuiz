33afecafaa21bf2ab51290db9314e79227c40dd2
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-14 00:20:11 +0100

    Created dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/Home"
 import RegisterPage from "./pages/Register"
 import LoginPage from "./pages/Login"
-
+import DashboardPage from "./pages/Dashboard"
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage/>} />
         <Route path="/register" element={<RegisterPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/dashboard" element={<DashboardPage />}/>
       </Routes>
     </div>
   )

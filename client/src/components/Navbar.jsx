d8fb85537e23b1020b08ee4659fe26e599865910
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-14 00:20:21 +0100

    made logo a link to home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,9 @@ import Logo from "../assets/logo.png"
 export default function NavBar() {
     return (
         <div className="flex h-16 m-2 justify-around items-center">
+            <Link to="/">
             <img src={Logo} alt="PowerQuiz logo" className="h-16" />
+            </Link>
             <svg className="w-6 h-6 sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>

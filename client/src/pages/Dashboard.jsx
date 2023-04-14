439d50c2cb778c9c68f57072c81c0b3fd80c7f7d
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-04-14 19:05:24 +0100

    moved quiz card and refactor functions

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,42 +1,51 @@
-import { Link } from "react-router-dom"
 import NavBar from "../components/Navbar"
 import DeletePortal from "../components/DeletePortal"
-import BackgroundImage from "../images/backgroundquiz.png"
+import QuizCard from "../components/QuizCard"
+
+import { Link, useNavigate } from "react-router-dom"
 import { useEffect, useRef, useState } from "react"
 
+import { isUserLoggedIn } from "../utility/user"
 
 export default function DashboardPage() {
+    const navigator = useNavigate()
+    const [quizzes, setQuizzes] = useState([])
     const [portalOpen, setPortalOpen] = useState(false)
     const promptedQuizDeleteId = useRef(null)
 
+    /**
+     *  Quiz object
+     * 
+     *  name
+     *  responses
+     *  id
+     */
+
     useEffect(() => {
         // GET quizzes
-        
-        /* 
-            If no cookie gets sent along, send back unauthorized and redirect to login page and
-            If cookie is not valid then return and clear cookie, then redirect to login page
-
-            If auth, then return quizzes to user.
-        */
-        
+        if (isUserLoggedIn()) {
+            navigator("/login")
+            return
+        }       
     }, [])
     
     // close portal, open portal when del is clicked
-    function onPortalClose() {
+    const onPortalClose = () => {
         setPortalOpen(false)
         console.log("Modal closed, delete aborted.")
     }
 
-    function promptDeleteQuiz(e) {
+    const promptDeleteQuiz = (e) => {
        // promptedQuizDeleteId.current = {some sort of uid}
         setPortalOpen(true)
     }
 
-    function portalDelete(e) {
+    const portalDelete = (e) => {
         setPortalOpen(false)
         // run logic to delete the quiz
         console.log("Deleted quiz")
        
+        // stop event from bubbling up
         e.stopPropagation()
     }
 
@@ -58,25 +67,9 @@ export default function DashboardPage() {
                 <h1 className="font-medium">View Quizzes</h1>
                 <DeletePortal open={portalOpen} closeModal={onPortalClose} onDelete={portalDelete}/>
                 
-                <div className="mt-4 flex flex-wrap gap-8">
-                    <Link className="flex flex-col min-w-xs rounded-xl border-2 
-                    min-h-[230px] min-w-[200px] overflow-hidden border-slate-300 hover:border-slate-700 transition-colors">
-                        <div className="bg-main h-[60%] w-60 overflow-clip">
-                            <img className="h-full w-full object-cover" src={BackgroundImage} alt="" />
-                        </div>
-                        <div className="flex bg-white flex-grow items-center justify-between pl-4 pr-8">
-                            <div className=" flex flex-col justify-center">
-                                <h1 className="font-medium">1999 Quiz</h1>
-                                <p className="text-sm">22 Responses</p>
-                            </div>
-                            <button onClick={promptDeleteQuiz} className="transition-colors hover:text-red-500" href="">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                </svg>
-                            </button>
-                        </div>
-                    </Link>
-                </div>
+                {quizzes.map(quiz => { 
+                    <QuizCard />
+                })}
             </div>
         </>
     )

824d2eb6ca572e9fa0a451b629b5120ec591f62b
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-05-10 22:54:55 +0100

    changed responses to be uniform across server

--- server/src/controllers/quizController.js
+++ server/src/controllers/quizController.js
@@ -3,23 +3,28 @@ import jwt from "jsonwebtoken"
 export const getAllQuizzes = (req, res) => {
     const session = req.cookies.session
     if (!session) {
-        return res.status(401).json("Client does not have required cookie.")
+        return res.status(401).json({message: "Client does not have required cookie."})
     }
 
+    let token
+
     try {
-        const verifiedToken = jwt.verify(session, process.env.TOKEN_KEY) 
         // this will check the token is valid, will also check that it is not expired and will return content in payload
-        console.log(verifiedToken)
+        token = jwt.verify(session, process.env.TOKEN_KEY) 
     } catch(err) {
+       
         if (err.name === "TokenExpiredError" || err.name === "JsonWebTokenError") {
             return res.clearCookie("session").status(401).json(err.message).redirect(`${process.env.CLIENT_ADDRESS}/login`)
         }
 
         console.log(err)
-        return res.clearCookie("session").status(500).json("A server error has occurred.")
+        return res.clearCookie("session").status(500).json({message: "A server error has occurred."})
     }
-    
+       
     // return quizzes
+    const userEmail = token.email
+    
 
-    res.status(200).json("Ok")
+    return  res.status(200).json({message: "Ok"})
+
 }

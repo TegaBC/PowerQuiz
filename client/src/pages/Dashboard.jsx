4e84b3b82b019fc1ab7f51709b3febbd4264071b
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-05-14 02:50:07 +0100

    directing dashboard links

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -59,7 +59,7 @@ export default function DashboardPage() {
             <div className="flex flex-col bg-slate-100 py-8 px-16 items-center h-screen">
                 <p className="mb-8 font-light text-3xl">Hello, {user.name}</p>
                 <div className="mb-8">
-                    <Link className="bg-main p-3 rounded-xl text-white font-semibold flex justify-center 
+                    <Link to="/quiz/create" className="bg-main p-3 rounded-xl text-white font-semibold flex justify-center 
                     gap-2 max-w-[170px] transition-colors hover:bg-main2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />

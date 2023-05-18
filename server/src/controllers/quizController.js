fb7d610dc303e398e484b6deda2fd6d7f35f2d3f
TegaBC <jjbencarew10@gmail.com>
2023-05-18 13:40:11 +0100

    added getting quizzes and framed out creating quiz

--- server/src/controllers/quizController.js
+++ server/src/controllers/quizController.js
@@ -1,24 +1,30 @@
 import verifySession from "../util/verifySession.js"
+import quizModel from "../models/quizModel.js"
 
-export const getAllQuizzes = (req, res) => {
+//finds all quizzes associated with the email and returns them to client
+export const getAllQuizzes = async (req, res) => {
     const token = verifySession(req.cookies.session)
     if(!token) return res.clearCookie("session").status(401).json({message: "Session could not be authorized"})
        
-    //TODO: look for user via email and return all quizzes
-    const userEmail = token.email
+    const quizzes = await quizModel.find({email: token.email})
 
-    return  res.status(200).json({message: "Ok"})
+    return res.status(200).json(quizzes)
 }
 
+//TODO: Create a new quiz after completing all checks
 export const createNewQuiz = (req, res) => {
     const token = verifySession(req.cookies.session)
     if(!token) return res.clearCookie("session").status(401).json({message: "Session could not be authorized"})
-      
 
-    // check it is a valid token
-    // check the user exists
-    // check the quiz has atleast 1 question
-    // check that option questions have an answer
-    
+    const quiz = req.body
+    const name = quiz.name
+    const questions = quiz.questions
+
+    if (!name || !questions || name === "") return res.status(400).json({message: "Quiz name or questions do not exist"})
+    if (!questions.isArray()) return res.status(400).json({message: "Questions post format is malformed"})
+
+    // save to database under the email of the user
+
+    // if created successfully it should redirect to the page of the quiz
     return  res.status(200).json({message: "Ok"})
 }

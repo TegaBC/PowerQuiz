634e60a37e3d893d8555c330b509b2b0ab95b774
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-18 23:09:18 +0100

    check for questions and quiz db

--- server/src/controllers/quizController.js
+++ server/src/controllers/quizController.js
@@ -23,7 +23,7 @@ export const createNewQuiz = async (req, res) => {
     const questions = quiz.questions
 
     if (!name || !questions || name === "") return res.status(400).json({message: "Quiz name or questions do not exist"})
-    if (!Array.isArray(questions)) return res.status(400).json({message: "Questions post format is malformed"})
+    if (!Array.isArray(questions) || questions.length == 0) return res.status(400).json({message: "Questions post format is malformed or has no quiz has no questions"})
 
     for (const question of questions) {
         if (question.type == "option" && !question.answer && !question.answer == 0) { // Make sure that we have the correct answer, 0 is valid
@@ -32,11 +32,11 @@ export const createNewQuiz = async (req, res) => {
     }
 
     try {
-      //  await quizModel.create({
-       //     owner: token.email,
-        //    name: name,
-         //   questions: JSON.stringify(questions)
-        //})
+        await quizModel.create({
+            owner: token.email,
+            name: name,
+            questions: JSON.stringify(questions)
+        })
         
         return res.status(200).json({message: "Quiz created."})
     } catch (err) {

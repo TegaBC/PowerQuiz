521802664f8edb2af933aefd42ac6ca729b01834
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-19 03:43:08 +0100

    added responses to model

--- server/src/models/quizModel.js
+++ server/src/models/quizModel.js
@@ -13,11 +13,13 @@ const quizSchema = new mongoose.Schema({
         type: String,       // JSON format
         required: true,
     },
+    responses: {
+        type: String,       // JSON format
+        required: true,
+    }
 })
 
-
-/**
-  
+/*
     QUESTIONS BODY EXAMPLE:
   
     {
@@ -26,11 +28,22 @@ const quizSchema = new mongoose.Schema({
         answer: 0
     }
 
-      {
+    {
         question: example? // this is a text answer question
         options: null
         answer: null
     }
- */
+*/
+
+/*
+    Response Example:
+  
+    {
+        name: "John"
+        answers: ["Text answer", 2]
+
+        >> Index of answer is the question, and for choice questions the answer will be stored as the index of the available choices
+    }
+*/
 
 export default mongoose.model("quiz", quizSchema)

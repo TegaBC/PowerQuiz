8c18c484b1efaa25e3983dc2971becc2ad131294
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-20 16:32:45 +0100

    added portals to fix zindex fighting

--- client/src/components/DeletePortal.jsx
+++ client/src/components/DeletePortal.jsx
@@ -1,9 +1,11 @@
+import ReactDom from "react-dom"
+
 export default function DeletePortal( { open, closeModal, onDelete } ) {
     if (!open) return null
     
     // Add mouse hover onto to svg to act like it has an effect when we are using div event instead.
 
-    return (
+    return ReactDom.createPortal(
         <div onClick={closeModal} className="mouse fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-60 flex justify-center items-center">
             <svg className="cursor-pointer fixed top-20 right-20 w-12 h-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -15,6 +17,5 @@ export default function DeletePortal( { open, closeModal, onDelete } ) {
                     Confirm
                 </button>
             </div>
-        </div>
-    )
+        </div>, document.getElementById("portal"))
 }

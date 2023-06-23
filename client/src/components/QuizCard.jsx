b291ab40b6eb486306ca4e9efab36706fef4b4ef
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-23 05:14:58 +0100

    added copying quiz url to clipboard

--- client/src/components/QuizCard.jsx
+++ client/src/components/QuizCard.jsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom"
 
 export default function QuizCard( { quiz, deleteClick } ) {
 
+    async function copyToClipboard(id) {
+        const domain = window.location.host
+        const quizUrl = `${domain}/quiz/${quiz._id}`
+        await navigator.clipboard.writeText(quizUrl)
+        alert("Copied quiz to clipboard")
+    }
+
     return (
         <div className="mt-4 flex flex-wrap gap-8">
             <div className="flex flex-col min-w-xs rounded-xl border-2 
@@ -15,11 +22,18 @@ export default function QuizCard( { quiz, deleteClick } ) {
                         <h1 className="font-medium">{quiz.name}</h1>
                         <p className="text-sm">{quiz.responses.length} Responses</p>
                     </div>
-                    <button onClick={deleteClick} className="transition-colors hover:text-red-500" href="">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                        </svg>
-                    </button>
+                    <div className="flex flex-col gap-2">
+                        <button onClick={deleteClick} className="transition-colors hover:text-red-500" href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                            </svg>
+                        </button>
+                        <button onClick={() => copyToClipboard(quiz._id)} className="transition-colors hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
